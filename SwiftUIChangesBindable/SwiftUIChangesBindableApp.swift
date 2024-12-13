53179637e9774a19969bbd8b1471cd85201cf24e
Lori Rothermel <lori@rothermel1.com>
2024-12-12 17:53:25 -0800

    Initial Commit

---
+++ SwiftUIChangesBindable/SwiftUIChangesBindableApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChangesBindableApp.swift
+//  SwiftUIChangesBindable
+//
+//  Created by Lori Rothermel on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChangesBindableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
