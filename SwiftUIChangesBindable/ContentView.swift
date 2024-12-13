7c17cec0b10f76f2844fcf37b1c32549717e6c4c
Lori Rothermel <lori@rothermel1.com>
2024-12-12 18:18:05 -0800

    Bindable example

--- SwiftUIChangesBindable/ContentView.swift
+++ SwiftUIChangesBindable/ContentView.swift
@@ -6,18 +6,40 @@
 //
 
 import SwiftUI
+import SwiftData
+
+@Observable
+class Light {
+    var isOn: Bool = false
+}
+
+struct Room: View {
+    @Bindable var light: Light
+        
+    var body: some View {
+        Toggle(isOn: $light.isOn) {
+            EmptyView()
+        }  // Toggle
+        .fixedSize()
+       
+    }  // some View
+    
+}  // Room
+
 
 struct ContentView: View {
+    @State private var light: Light = Light()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            Room(light: light)
+        }  // VStack
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(light.isOn ? .yellow : .black)
+        
+    }  // some View
+    
+}  // ContentView
 
 #Preview {
     ContentView()
